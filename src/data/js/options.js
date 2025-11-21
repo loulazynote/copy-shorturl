d209704dac93aaefd3b41d6c4a549bbeaeb5b3c0
Lou Chang <louchang.tw@gmail.com>
2025-11-20 23:47:01 -0500

    Added: t.ly shortener and correct locales messages

--- src/data/js/options.js
+++ src/data/js/options.js
@@ -1,105 +1,112 @@
 const options = {
-  service: {
-    attr: 'value',
-    def: 'isgd'
-  },
-  bitly_apikey: {
-    attr: 'value',
-    def: ''
-  },
-  kuttit_apikey: {
-    attr: 'value',
-    def: ''
-  },
-  kuttit_domain: {
-    attr: 'value',
-    def: ''
-  },
-  custom_url: {
-    attr: 'value',
-    def: ''
-  },
-  shorten_canonical: {
-    attr: 'value',
-    def: 30
-  },
-  use_special: {
-    attr: 'checked',
-    def: true
-  },
-  strip_urm: {
-    attr: 'checked',
-    def: true
-  },
-  keep_hash: {
-    attr: 'checked',
-    def: false
-  },
-  notify: {
-    attr: 'checked',
-    def: true
-  },
-  copy_title: {
-    attr: 'checked',
-    def: false
-  }
-}
+    service: {
+        attr: 'value',
+        def: 'isgd',
+    },
+    bitly_apikey: {
+        attr: 'value',
+        def: '',
+    },
+    kuttit_apikey: {
+        attr: 'value',
+        def: '',
+    },
+    kuttit_domain: {
+        attr: 'value',
+        def: '',
+    },
+    tly_apikey: {
+        attr: 'value',
+        def: '',
+    },
+    tly_domain: {
+        attr: 'value',
+        def: '',
+    },
+    custom_url: {
+        attr: 'value',
+        def: '',
+    },
+    shorten_canonical: {
+        attr: 'value',
+        def: 30,
+    },
+    use_special: {
+        attr: 'checked',
+        def: true,
+    },
+    strip_urm: {
+        attr: 'checked',
+        def: true,
+    },
+    keep_hash: {
+        attr: 'checked',
+        def: false,
+    },
+    notify: {
+        attr: 'checked',
+        def: true,
+    },
+    copy_title: {
+        attr: 'checked',
+        def: false,
+    },
+};
 let savedTimeout; // Hide saved indicator after a bit.
 
-
 function init() {
-  // Initialize i18n.
-  document.querySelectorAll('[data-i18n]').forEach(node => {
-    node.textContent = browser.i18n.getMessage(node.dataset.i18n);
-  });
-
-  // Restore settings.
-  browser.storage.local.get('prefs').then(res => {
-    let currentPrefs = res['prefs'] || {};
-    Object.keys(options).forEach(id => {
-      let val = (typeof currentPrefs[id] !== 'undefined') ? currentPrefs[id] : options[id].def;
-      document.getElementById(id)[options[id].attr] = val;
+    // Initialize i18n.
+    document.querySelectorAll('[data-i18n]').forEach((node) => {
+        node.textContent = browser.i18n.getMessage(node.dataset.i18n);
     });
 
-    showHideDetails();
-  });
+    // Restore settings.
+    browser.storage.local.get('prefs').then((res) => {
+        let currentPrefs = res['prefs'] || {};
+        Object.keys(options).forEach((id) => {
+            let val = typeof currentPrefs[id] !== 'undefined' ? currentPrefs[id] : options[id].def;
+            document.getElementById(id)[options[id].attr] = val;
+        });
 
-  document.querySelector('#service').addEventListener('change', showHideDetails);
+        showHideDetails();
+    });
+
+    document.querySelector('#service').addEventListener('change', showHideDetails);
 }
 
 function showHideDetails() {
-  // Show / hide details depending on service selection.
-  let selected = document.querySelector('#service').value;
+    // Show / hide details depending on service selection.
+    let selected = document.querySelector('#service').value;
 
-  ['bitly', 'kuttit', 'custom'].forEach(service => {
-    document.querySelector('#' + service + '_details').style.display = (selected === service) ? 'block': 'none';
-  });
+    ['bitly', 'kuttit', 'tly', 'custom'].forEach((service) => {
+        document.querySelector('#' + service + '_details').style.display = selected === service ? 'block' : 'none';
+    });
 
-  // Max canonical URL length makes no sense with no shortening service.
-  document.querySelector('#always_shorten').style.display = (selected === 'none') ? 'none': 'block';
+    // Max canonical URL length makes no sense with no shortening service.
+    document.querySelector('#always_shorten').style.display = selected === 'none' ? 'none' : 'block';
 }
 
 function saveOptions(e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  let toSave = {prefs: {}};
-  Object.keys(options).forEach(id => {
-    toSave['prefs'][id] = document.getElementById(id)[options[id].attr];
-  });
-  browser.storage.local.set(toSave).then(() => {
-    // Indicate that we saved succesfully.
-    let saved = document.querySelector('#saved');
+    let toSave = { prefs: {} };
+    Object.keys(options).forEach((id) => {
+        toSave['prefs'][id] = document.getElementById(id)[options[id].attr];
+    });
+    browser.storage.local.set(toSave).then(() => {
+        // Indicate that we saved succesfully.
+        let saved = document.querySelector('#saved');
 
-    saved.style.display = 'block';
+        saved.style.display = 'block';
 
-    if (savedTimeout) {
-      clearTimeout(savedTimeout);
-    }
-    savedTimeout = setTimeout(() => {
-      saved.style.display = 'none';
-      savedTimeout = null;
-    }, 3000)
-  });
+        if (savedTimeout) {
+            clearTimeout(savedTimeout);
+        }
+        savedTimeout = setTimeout(() => {
+            saved.style.display = 'none';
+            savedTimeout = null;
+        }, 3000);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
